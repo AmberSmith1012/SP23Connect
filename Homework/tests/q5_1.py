fe4099426b86784f5632838a81cc9f5e31c2e20a
Amber Smith <amber.smith@ncssm.edu>
2023-01-24 19:51:43 +0000

    Fixing Homework01

--- Homework/tests/q5_1.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q5_1',
-    'points': None,
-    'suites': [   {   'cases': [{'code': '>>> isinstance(biggest_change, (int, float, np.float, np.float64))\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
--- Homework/tests/q5_2.py
+++
@@ -1,7 +0,0 @@
-test = {   'name': 'q5_2',
-    'points': [0],
-    'suites': [   {   'cases': [{'code': '>>> isinstance(smallest_change_course, (int, float, np.float, np.float64))\nTrue', 'hidden': False, 'locked': False}],
-                      'scored': True,
-                      'setup': '',
-                      'teardown': '',
-                      'type': 'doctest'}]}
